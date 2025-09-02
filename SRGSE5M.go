ac1c0142d81b04a64361d397933f5a7280208b99
“Chouette2100” <chouette.21.00@gmail.com>
2025-09-02 22:20:08 +0900

    021BA01    main()でのdefer srdblib.Dbmap.Db.Close()のもれを補う

--- SRGSE5M.go
+++ SRGSE5M.go
@@ -183,13 +183,15 @@ import (
 	Ver. 021AZ01	シグナルを検出したときのメッセージを実態に合わせる。main.goをmain.goとInsertIntoPoints.goに分離する。
 	Ver. 021AZ02	srdblib.Dberrをすべてerrとする
 	Ver. 021BA00    GetPointsAll()でのイベント終了時の検出を獲得ポイント取得時にも行う。
+	Ver. 021BA01    main()でのdefer srdblib.Dbmap.Db.Close()のもれを補う
+
 
 	課題
 		登録済みの開催予定イベントの配信者がそれを取り消し、別のイベントに参加した場合scoremapを使用した処理に問題が生じる
 
 */
 
-const version = "021BA00"
+const version = "021BA01"
 
 const Maxroom = 10
 const ConfirmedAt = 59 //	イベント終了時刻からこの秒数経った時刻に最終結果を格納する。
@@ -372,6 +374,7 @@ func main() {
 	//	gorpの初期設定を行う
 	dial := gorp.MySQLDialect{Engine: "InnoDB", Encoding: "utf8mb4"}
 	srdblib.Dbmap = &gorp.DbMap{Db: srdblib.Db, Dialect: dial, ExpandSliceArgs: true}
+	defer srdblib.Dbmap.Db.Close() // gorpのDB接続をクローズ
 
 	srdblib.Dbmap.AddTableWithName(srdblib.User{}, "user").SetKeys(false, "Userno")
 	srdblib.Dbmap.AddTableWithName(srdblib.Userhistory{}, "userhistory").SetKeys(false, "Userno", "Ts")
